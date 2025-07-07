0241399f6eafdb5272674b844d61e4137730b3e0
Ganthan.T.M <tmganthan@gmail.com>
2025-07-07 11:56:38 +0530

    first-commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-xl text-red-500'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
---
+++ server/src/test/java/com/server/AIResumeBuilder/AiResumeBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.AIResumeBuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiResumeBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
